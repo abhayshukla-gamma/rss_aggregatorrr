f80763dddadec0cb8610df87f94bf7a85f6176be
abhayshukla-gamma <abhay.shukla@gammaedge.io>
2026-02-13 16:19:36 +0530

    html templates

--- app/main.py
+++ app/main.py
@@ -3,17 +3,23 @@ from app.models import feed , item
 from app.core.database import Base,engine
 from app.routers import feed_router
 from app.services.scheduler_service import start_scheduler
-
+from fastapi.templating import Jinja2Templates
+from app.routers import ui_router
 
 app = FastAPI()
+templates = Jinja2Templates(directory="app/templates")     # means thr html files are present in app/templates 
 
 @app.on_event("startup")
 def startup_event():
     start_scheduler()
+app.include_router(ui_router.router)
 
 app.include_router(feed_router.router)
 # Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
+
+
+
  # to tell python to create table in a database 
 
 # @app.get("/")
---
+++ app/routers/ui_router.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter, Request, Depends
+from sqlalchemy.orm import session 
+from fastapi.templating import Jinja2Templates
+from app.core.database import get_db
+from app.models.feed import Feed
+from app.models.item import Item
+
+router = APIRouter(tags=["UI"])
+templates = Jinja2Templates(directory="app/templates")
+
+@router.get("/")
+def home(request : Request):
+    return templates.TemplateResponse("home.html", {"request" : request})     # request means we are passingt the templates # home.html means which html file ,request:Request means jinja2 needs it 
+
+@router.get("/ui/feeds")
+def ui_feeds(request: Request, db: Session = Depends(get_db)):
+    feeds = db.query(Feed).all()
+
+    return templates.TemplateResponse(
+        "feeds.html",
+        {
+            "request": request,
+            "feeds": feeds
+        }
+    )
+
+@router.get("/ui/feeds/{feed_id}/items")
+def ui_feed_items(feed_id: int, request: Request, db: Session = Depends(get_db)):
+    items = db.query(Item).filter(Item.feed_id == feed_id).all()
+
+    return templates.TemplateResponse(
+        "items.html",
+        {
+            "request": request,
+            "items": items
+        }
+    )
+
